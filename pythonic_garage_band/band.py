b743a52426eaf309ddf35a3c2bb727a4dfb847fc
Ashrf Mathkour Obeidat <ashrfmathkour@gmail.com>
2021-10-13 18:35:12 +0300

    test2: completed the repr function

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -21,3 +21,5 @@ class Guitarist:
             self.name=name
       def __str__(self):
             return (f'My name is {self.name} and I play guitar')
+      def __repr__(self):
+            return (f'Guitarist instance. Name = {self.name}')
