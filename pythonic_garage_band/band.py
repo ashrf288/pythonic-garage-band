8310e3e4462279ddadb7485e4b5f780415ccd775
Ashrf Mathkour Obeidat <ashrfmathkour@gmail.com>
2021-10-13 23:18:19 +0300

    a

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -1,11 +1,16 @@
 
-
+from abc import abstractclassmethod
 
 class Band:
-  
-   def __init__(self,name,members  ):
+   '''
+      this class has (name and member )
+      member is an array which we add to it every band member once we creat new object
+      (calls init fun) 
+   '''
+   members=[]
+   def __init__(self,name,memberName):
          self.name =name 
-         self.members=members
+         Band.members.append(memberName)
 
 
    def play_solos(self):
@@ -14,7 +19,10 @@ class Band:
     
    def to_list(self):
     print("Hello my name is " + self.name)
+   
 
+
+ 
 
 class Guitarist:
       def __init__ (self,name):
@@ -25,6 +33,8 @@ class Guitarist:
             return (f'Guitarist instance. Name = {self.name}')
       def get_instrument(self):
           return 'guitar'
+      def play_solos (self):
+          return 'face melting guitar solo'
 
 class Drummer(Guitarist):
        def __str__(self):
@@ -33,6 +43,10 @@ class Drummer(Guitarist):
             return (f'Drummer instance. Name = {self.name}')
        def get_instrument(self):
           return 'drums'
+       def play_solos (self):
+          return 'rattle boom crash'
+
+ 
 class Bassist(Guitarist):
        def __str__(self):
             return (f'My name is {self.name} and I play bass')
@@ -40,3 +54,36 @@ class Bassist(Guitarist):
             return (f'Bassist instance. Name = {self.name}')
        def get_instrument(self):
           return 'bass'
+       def play_solos (self):
+          return 'bom bom buh bom'
+
+
+class Musician:
+    members = []
+
+    def init(self, name):
+
+        self.name = name
+        Musician.members.append(self)
+
+    @abstractclassmethod
+    def str(self):
+        pass
+
+    @abstractclassmethod
+    def repr(self):
+        pass
+
+    @abstractclassmethod
+    def get_instrument(self):
+        pass
+
+    @abstractclassmethod
+    def play_solo(self):
+        pass
+
+
+
+
+ash=Drummer('ash')
+print(ash.get_instrument())
