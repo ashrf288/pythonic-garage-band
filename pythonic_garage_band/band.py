367d3e74ef0c5ddbbbec7ba608bc41b65878a1b3
Ashrf Mathkour Obeidat <ashrfmathkour@gmail.com>
2021-10-13 18:39:18 +0300

    test : drummer repr function added

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -27,3 +27,5 @@ class Guitarist:
 class Drummer(Guitarist):
        def __str__(self):
             return (f'My name is {self.name} and I play drums')
+       def __repr__(self):
+            return (f'Drummer instance. Name = {self.name}')
